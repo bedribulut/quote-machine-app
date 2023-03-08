365f54cdc9901e3a98d7f47e698c76f77d442d22
bedri bulut <bedriytu@gmail.com>
2023-03-08 11:00:08 +0300

    i hope it to be last change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 let quoteDB = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 function App() {
-  const [quote, setQuote] = useState('initialState');
-  const [author, setAuthor] = useState('Charles');
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
   const [randomNumber, setRandomNumber] = useState(0);
   const [quotesArray, setQuotesArray] = useState(null);
   const [accentColor, setAccentColor] = useState('#282c34');
