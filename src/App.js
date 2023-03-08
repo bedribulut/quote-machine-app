47aa3b8e939d59acb1749e75fd9e7dd3482b4c2c
bedri bulut <bedriytu@gmail.com>
2023-03-08 11:37:12 +0300

    hoping it to be last again

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 let quoteDB = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 function App() {
-  const [quote, setQuote] = useState(null);
-  const [author, setAuthor] = useState(null);
+  const [quote, setQuote] = useState('if one room is 20TRY then whole flat would be 75TRY.');
+  const [author, setAuthor] = useState('Yılmaz');
   const [randomNumber, setRandomNumber] = useState(0);
   const [quotesArray, setQuotesArray] = useState(null);
   const [accentColor, setAccentColor] = useState('#282c34');
